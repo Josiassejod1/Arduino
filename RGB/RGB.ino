5fc2761958c748e42c269360f135dad31a4331f1
Dalvin Josias Sejour <dalvin.josias@gmail.com>
2025-12-17 23:11:26 -0500

    more fun

---
+++ RGB/RGB.ino
@@ -0,0 +1,22 @@
+#define BLUE 3
+#define GREEN 5
+#define RED 6
+
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  digitalWrite(RED, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(BLUE, HIGH);
+}
+
+
+
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
